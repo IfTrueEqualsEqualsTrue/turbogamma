dbf0f9cd5fb2a408eb53fca159c51e39546a3ac7
Guillaume <xgold3nfir3@gmail.com>
2026-08-12 17:09:29 +0200

    added gamma_2d and 2d visualizer.py

--- src/turbogamma/gamma.py
+++ src/turbogamma/gamma.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import numpy as np
 
-DOSE_TOLERANCE = 0.02
+DOSE_TOLERANCE_PERCENT = 0.02
 DTA = 2
 
 
@@ -20,12 +20,37 @@ class GammaResult:
 
 
 def gamma_1d(ref_grid: DoseGrid, eval_grid: DoseGrid) -> GammaResult:
+    dose_tolerance_abs = DOSE_TOLERANCE_PERCENT * ref_grid.dose.max()
     ref_pos_row = ref_grid.coordinates[0]
     ref_pos_col = ref_pos_row.reshape((len(ref_pos_row), 1))
     eval_pos_row = eval_grid.coordinates[0]
     ref_dose_col = ref_grid.dose.reshape((len(ref_pos_row), 1))
     dose_diff = ref_dose_col - eval_grid.dose
     pos_diff = ref_pos_col - eval_pos_row
-    gammas: np.ndarray = np.sqrt((dose_diff / DOSE_TOLERANCE) ** 2 + (pos_diff / DTA) ** 2)
+    gammas: np.ndarray = np.sqrt((dose_diff / dose_tolerance_abs) ** 2 + (pos_diff / DTA) ** 2)
     gamma = gammas.min(axis=1)
+    return GammaResult(ref_grid, eval_grid, gamma)
+
+
+def gamma_2d(ref_grid: DoseGrid, eval_grid: DoseGrid) -> GammaResult:
+    # Position first
+    xx_pos_ref, yy_pos_ref = np.meshgrid(ref_grid.coordinates[0], ref_grid.coordinates[1], indexing='ij')
+    pos_ref_flatten = np.stack([xx_pos_ref.flatten(), yy_pos_ref.flatten()], axis=-1)
+
+    xx_pos_eval, yy_pos_eval = np.meshgrid(eval_grid.coordinates[0], eval_grid.coordinates[1], indexing='ij')
+    pos_eval_flatten = np.stack([xx_pos_eval.flatten(), yy_pos_eval.flatten()], axis=-1)
+
+    dist_diff = pos_ref_flatten[:, None, :] - pos_eval_flatten
+    sq_dist = (dist_diff ** 2).sum(axis=-1)
+
+    # Dose then
+
+    dose_ref_flatten = ref_grid.dose.flatten()
+    dose_eval_flatten = eval_grid.dose.flatten()
+    dose_diff = dose_ref_flatten[:, None] - dose_eval_flatten
+
+    dose_tolerance_abs = DOSE_TOLERANCE_PERCENT * ref_grid.dose.max()
+
+    gammas = np.sqrt((dose_diff / dose_tolerance_abs) ** 2 + (np.sqrt(sq_dist) / DTA) ** 2)
+    gamma = gammas.min(axis=1).reshape(ref_grid.dose.shape)
     return GammaResult(ref_grid, eval_grid, gamma)
--- src/turbogamma/scenarios.py
+++ src/turbogamma/scenarios.py
@@ -5,24 +5,56 @@ import numpy as np
 from turbogamma.gamma import DTA, DoseGrid
 
 
-def dose_ramp(i: int, dose: float) -> float:
-    return dose * i
+class Scenarios1d:
+
+    @staticmethod
+    def build_zeros(size: int) -> DoseGrid:
+        return DoseGrid(coordinates=(np.zeros(size),), dose=np.zeros(size))
+
+    @staticmethod
+    def build_constant_dose(size: int, dose: float) -> DoseGrid:
+        return DoseGrid(coordinates=(np.zeros(size),),
+                        dose=np.full(size, dose))
+
+    @staticmethod
+    def build_uniform_feature(size: int, constant_dose: float) -> Callable:
+        def _make(shift: int) -> DoseGrid:
+            i = np.arange(size)
+            pos = DTA * i
+
+            feature_width = size // 2
+            start = (size - feature_width) // 2
+            mask = (i >= start + shift) & (i < start + shift + feature_width)
+            dose = np.where(mask, constant_dose, 0.0)
+
+            return DoseGrid((pos,), dose)
+
+        return _make
 
 
-def build_zeros(size: int) -> DoseGrid:
-    return DoseGrid(coordinates=(np.zeros(size),), dose=np.zeros(size))
+class Scenarios2d:
 
+    @staticmethod
+    def build_zeros(size: int) -> DoseGrid:
+        return DoseGrid(coordinates=(np.zeros(size), np.zeros(size)), dose=np.zeros((size, size)))
 
-def build_constant_dose(size: int, dose: float) -> DoseGrid:
-    return DoseGrid(coordinates=(np.zeros(size),),
-                    dose=np.full(size, dose))
+    @staticmethod
+    def build_constant_dose(size: int, dose: float) -> DoseGrid:
+        return DoseGrid(coordinates=(np.zeros(size), np.zeros(size)),
+                        dose=np.full((size, size), dose))
 
+    @staticmethod
+    def build_uniform_feature(size: int, constant_dose: float) -> Callable:
+        def _make(shift: int) -> DoseGrid:
+            i = np.arange(size)
+            ix, iy = np.meshgrid(i, i, indexing="ij")
+            pos = DTA * i
 
-def build_dose_ramp(size: int, constant_dose: float) -> Callable:
-    def _make(shift: int) -> DoseGrid:
-        i = np.arange(size)
-        pos = DTA * (i + shift)
-        dose = dose_ramp(i, constant_dose)
-        return DoseGrid((pos,), dose)
+            feature_width = size // 2
+            start = (size - feature_width) // 2
+            mask = (ix >= start + shift) & (iy >= start + shift) & (ix < start + shift + feature_width) & (
+                        iy < start + shift + feature_width)
+            dose = np.where(mask, constant_dose, 0.0)
+            return DoseGrid((pos, pos,), dose)
 
-    return _make
+        return _make
